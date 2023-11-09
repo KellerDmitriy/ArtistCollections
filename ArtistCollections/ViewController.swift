8d5a6a6684eaf9f1a0d47712ecc8bd5e0ebfb1b6
DmKeller <dmit.keller@gmail.com>
2023-11-09 13:55:47 +0300

    Initial Commit

---
+++ ArtistCollections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtistCollections
+//
+//  Created by Келлер Дмитрий on 09.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
